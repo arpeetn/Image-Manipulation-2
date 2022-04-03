c430d6e934a4d691e0cf1aed479877be73ab32f4
Arpeet Naik <99459904+arpeetn@users.noreply.github.com>
2022-04-03 18:53:56 -0400

    Add files via upload

---
+++ Image.h
@@ -0,0 +1,42 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <ostream>
+#include <vector>
+#include <fstream>
+#include "Pixel.h"
+#include "Header.h"
+
+using namespace std;
+
+class Image{
+  private:
+
+    Header header;
+    vector<Pixel> pixels;
+
+    vector<Pixel> read_pixels(const Header& header, ifstream& input);
+    Header read_header(ifstream& input);
+
+    Image& operator=(const Image& image);
+
+  public:
+
+
+    Image(const Image& input);
+    Image(ifstream& image);
+
+    ~Image();
+
+
+    Pixel& operator()(int x, int y);
+
+    void write_header(ofstream& output) const;
+    void write_to(ofstream& output) const;
+
+    const Header getHeader() const;
+    const vector<Pixel> getPixels() const;
+
+};
+
+#endif
