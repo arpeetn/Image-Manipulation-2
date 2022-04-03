09afec02ec93d40676ef2e0af1995b97f9897426
Arpeet Naik <99459904+arpeetn@users.noreply.github.com>
2022-04-03 18:54:56 -0400

    Add files via upload

---
+++ manip.h
@@ -0,0 +1,16 @@
+// Arpeet Naik
+// CPSC 1020 - Fall 2021
+// PA2
+
+#ifndef MANIP_H
+#define MANIP_H
+
+#include <ostream>
+using namespace std;
+#include "Image.h"
+
+void gray(Image&);
+void mirror(Image&);
+void green_screen(Image&, Image&);
+
+#endif
